6e320c00dad904388d245984e9f42b82b3c6122e
chem1st <reactive.90@mail.ru>
2015-10-22 19:04:43 +0400

    views rewritten - no ajax

--- quizext/views.py
+++ quizext/views.py
@@ -41,11 +41,16 @@ def startquiz(request, pk):
 	test = Test.objects.get(pk=pk)
 	attempt = Attempt(user=request.user, test=test, number=attempt_count)
 	attempt.save()
-	return HttpResponseRedirect(reverse('question', kwargs={'pk': pk, 'q_set': 1 }))
+	return HttpResponseRedirect(reverse('question', kwargs={'pk': pk, 'q_set': 1, 'attempt_count': attempt_count}))
 
-def question(request, pk, q_set, **kwargs):
+def question(request, pk, q_set, attempt_count):
+	try:
+		q = Question.objects.filter(test__pk=pk).filter(group=q_set).order_by('?').first()
+		next_q_set = Question.objects.filter(group__gt=q_set).order_by('group').first().group
+	except:
+		next_q_set = 0
 	if request.method == "POST":
-		attempt = Attempt.objects.get(user=request.user, test__pk=pk, number=kwargs['attempt_count'])
+		attempt = Attempt.objects.get(user=request.user, test__pk=pk, number=attempt_count)
 		correct = Answer.objects.filter(question__pk=request.POST.get('q_pk'), 
 			is_correct=True).values_list('id', flat=True)
 		checked = request.POST.getlist('answer')
@@ -62,35 +67,29 @@ def question(request, pk, q_set, **kwargs):
 		attempt.set_json(answers)
 		attempt.save()
 		if int(q_set) == 0:
-			return HttpResponseRedirect(reverse('results', kwargs={'pk': pk}))
-		c = {'question': q, 'next': next_q_set, 'attempt_count': request.POST.get('attempt_count'), 'answers': answers}
-	q = Question.objects.filter(test__pk=pk).filter(group=q_set).order_by('?').first()
-	try:
-		next_q_set = Question.objects.filter(group__gt=q_set).order_by('group').first().group
-	except:
-		next_q_set = 0
-	c = {'question': q, 'next': next_q_set }
+			return HttpResponseRedirect(reverse('results', kwargs={'pk': pk, 'attempt_count': attempt_count}))
+		c = {'question': q, 'next': next_q_set, 'attempt_count': attempt_count, 'answers': answers}
+	c = {'question': q, 'next': next_q_set, 'attempt_count': attempt_count}
 	return render(request, 'quizext/question.html', c)
 
 
 def results(request, pk, attempt_count):
-	c = {}
-	if request.method == "POST":
-		attempt = Attempt.objects.get(user=request.user, test__pk=pk, number=attempt_count)
-		q = Question.objects.filter(test__pk=pk)
-		q_count = q.values('group').distinct().count()
-		p_sum = q.aggregate(Sum('points')).values()[0]
-		user_points = 0
-		try:
-			answers = attempt.get_json()
-			for k,kv in answers.iteritems():
-				user_points += kv[1]
-		except ValueError:
-			pass
-		attempt.points = user_points
-		attempt.set_json(answers)
-		attempt.save()
-		user_percent = attempt.points / p_sum * 100
-		c = {'test_title': attempt.test.title, 'answers': answers, 'q_count': q_count, 'p_sum': p_sum, 'user_points': user_points, 
-			'user_percent': user_percent}
+	attempt = Attempt.objects.get(user=request.user, test__pk=pk, number=attempt_count)
+	print attempt
+	q = Question.objects.filter(test__pk=pk)
+	q_count = q.values('group').distinct().count()
+	p_sum = q.aggregate(Sum('points')).values()[0]
+	user_points = 0
+	try:
+		answers = attempt.get_json()
+		for k,kv in answers.iteritems():
+			user_points += kv[1]
+	except ValueError:
+		pass
+	attempt.points = user_points
+	attempt.set_json(answers)
+	attempt.save()
+	user_percent = attempt.points / p_sum * 100
+	c = {'test_title': attempt.test.title, 'answers': answers, 'q_count': q_count, 'p_sum': p_sum, 'user_points': user_points, 
+		'user_percent': user_percent}
 	return render(request, 'quizext/results.html', c)
